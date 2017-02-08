d98e9ed4d18e4d30d96e6181181485944a4b319a
prashanth-devadas <prashanth-devadas@users.noreply.github.com>
2017-02-09 00:25:21 +0530

    Correcting misspelled Constructor name
    Misspelled __construct as __contruct in the base Controller class. Committing after making this correction

--- Core/Controller.php
+++ Core/Controller.php
@@ -2,31 +2,29 @@
 
 namespace Core;
 
-/***
+/**
  * Base controller
- * 
- * PHP version 5.4
+ *
+ * PHP version 5.5.9
  */
 abstract class Controller
 {
 
-	/***
-	* Parameters from the matched route
-	* @var array
-	*/
-	protected $route_params = array();
+    /**
+     * Parameters from the matched route
+     * @var array
+     */
+    protected $route_params = array();
 
-	/***
-	* Class contructor
-	* 
-	* @param array $route_params Parameters from the route
-	*
-	* @return void
-	*/
-
-	public function __contruct($route_params)
-	{
-		$this->route_params = $route_params;
-
-	}
+    /**
+     * Class constructor
+     *
+     * @param array $route_params  Parameters from the route
+     *
+     * @return void
+     */
+    public function __construct($route_params)
+    {
+        $this->route_params = $route_params;
+    }
 }
