5aa171743d565b99b66f894db0541d89a7135980
Prashanth Devadas <prashanthharidas@yahoo.co.in>
2017-03-11 11:20:31 +0530

    Including Composer autoload

--- public/index.php
+++ public/index.php
@@ -18,13 +18,17 @@ Twig_Autoloader::register();
 /**
  * Autoloader
  */
-spl_autoload_register(function ($class) {
+
+/***
+	spl_autoload_register(function ($class) {
     $root = dirname(__DIR__);   // get the parent directory
     $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
     if (is_readable($file)) {
         require $root . '/' . str_replace('\\', '/', $class) . '.php';
     }
-});
+}); 
+
+*/
 
 
 /**
